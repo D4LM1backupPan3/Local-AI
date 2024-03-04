72b4e545398c74e08d935b402a064597a7be6bd9
D4LM <148903901+D4LM1backupPan3@users.noreply.github.com>
2024-03-04 13:46:18 -0500

    Changed file to fix issues

--- LocalAI.py
+++ LocalAI.py
@@ -1,53 +1,51 @@
-#Import random module
 import random
-import os
-import time
-import random
-print("Module in LocalAI.py imported")
+import json
+
+#Variables
+BotName = "Cortona"
+BotNameColon = "Cortona: "
 
 #Greetings and goodbyes
 greetings=["Hello!", "What's up!", "Howdy!", "Greetings!"]
 goodbyes=["Bye!","Goodbye!","See you later!","See you soon!"]
-print("Lists in LocalAI.py imported")
 
-#Conenctor for the "Hi" command
-from Randomizer import randomizer
-print("Imported the function randomizer from Randomizer.py.")
-randomizer()
-print("Randomizer function started.")
-time.sleep(1)
-print("Stopped the code for 1 second so that the code on the other side can run")
-from Randomizer import GreetingTemp
-print("Getting the variable from the other file, Randomizer.py")
+responseData = json.load(open("response.json", "r"))
+dictionaryResponseData = json.load(open("dictionary.json", "r"))
 
-#Keywords and responses that AI chatbot will know
-keywords = ["music", "pet", "book", "game", "minecraft", "Hi"]
-responses=["Music is so relaxing!", "Dogs are man's best friend!", "I know a lot of books.", "I like to play video games.","Minecraft is a great game!","Please continue, you did not enter anything", GreetingTemp]
-print("All lists form LocalAI.py loaded.")
+print(BotNameColon + random.choice(greetings) + " My name is Cortona!")
 
-#Info about bot
-BotName = "Cortona"
 
-print(random.choice(greetings) + " My name is " + BotName + ".")
-
-user = input("Say something or type bye to quit: ")
-user = user.lower()
-
-while (user !="bye"):
-    keyword_found = False
-
-    for index in range(len(keywords)):
-        if (keywords[index] in user):
-            print(BotName + ": " + responses[index])
-            keyword_found = True
+while True :
+   
+    userinp = input("").lower()
     
-    if (keyword_found == False):
-        new_keyword = input("I'm not sure how to respond. What keyword should I respond to?")
-        keywords.append(new_keyword)
-        new_response = input("How should I respond to" + new_keyword + "?")
-        responses.append(new_response)
+    if userinp == "hi" :
+        print(BotNameColon + random.choice(greetings))
+        continue
 
-    user = input("Say something or type bye to quit: ")
-    user = user.lower()
+    if userinp == "bye" :
+        print(BotNameColon + random.choice(goodbyes))
+        break
+    
+    if userinp == "dictionary":
+        print(BotNameColon + "Dictionary mode activated, type the word you want to find in english dictionary")
+        print(BotNameColon + "Printing whole dictionary")
+        print(dictionaryResponseData)
+        #while True:
+            #if userinp in responseData:
+                #print(BotNameColon + dictionaryResponseData[userinp])
 
-print(random.choice(goodbyes))
+
+    if userinp in responseData :
+        print(BotNameColon + responseData[userinp])
+        continue
+    
+    if not userinp in BotNameColon :
+        response = input(BotNameColon + "I've never seen this command, how should I respond? ")
+
+    responseData[userinp] = response
+    
+    f = open("response.json", "w")
+    json.dump(responseData, f)
+
+
--- Randomizer.py
+++
@@ -1,7 +0,0 @@
-def randomizer():
-    import random
-    import time
-    from LocalAI import greetings
-    GreetingTemp = random.choice(greetings)
-    time.sleep(5)
-    randomizer()
